f32d36a14a710ca808f0cd654a851e93f4254ebf
garamgim <garamgim@gmail.com>
2024-01-27 19:48:46 +0900

    Add solution 25314

---
+++ String/25314_코딩은체육과목입니다.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        n /= 4;
+
+        StringBuilder sb = new StringBuilder(6 * n);
+        for (int i = 0; i < n; i++) {
+            sb.append("long ");
+        }
+
+        System.out.println(sb.append("int").toString());
+    }
+}
