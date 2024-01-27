2fce54658ff740c44c2bfbe4792c29ae81432eca
garamgim <garamgim@gmail.com>
2024-01-27 19:56:17 +0900

    Update directory name

--- String/25314_코딩은체육과목입니다.java
+++ String/25314_코딩은체육과목입니다.java
@@ -1,16 +1,17 @@
+package String;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
         n /= 4;
-
         StringBuilder sb = new StringBuilder(6 * n);
         for (int i = 0; i < n; i++) {
             sb.append("long ");
         }
-
         System.out.println(sb.append("int").toString());
     }
 }
