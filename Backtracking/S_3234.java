9f8ca23d6dff820fc57c2bafed371f50837f0681
garamgim <garamgim@gmail.com>
2024-03-03 14:10:56 +0900

    Add solution S 3234

--- Backtracking/S_3234.java
+++ Backtracking/S_3234.java
@@ -1,12 +1,30 @@
+/*
+* SWEA 3234 준환이의 양팔저울
+*
+* https://data-make.tistory.com/490 답안 참고
+* static 변수를 사용하지 않고 함수에 파라미터를 일일이 넘겨주는 방식을 사용
+* 공간 효율성은 떨어지지만 시간 효율성은 올라갈 수 있다
+* 지역변수에 접근하는 것이 더 빠르기 때문
+* */
+
+/*
+* static 변수 미사용시 공간효율성이 떨어지는 이유?
+*
+* static 변수를 사용하면 하나의 변수가 모든 인스턴스에 의해 공유되므로 메모리 사용이 적지만
+* 인스턴스 변수는 각 인스턴스마다 별도의 변수가 있으므로 더 많은 메모리를 사용할 수 있다
+* https://withlearncode.com/entry/%EC%9E%90%EB%B0%94-%EC%9D%91%EC%9A%A9-Chapter-16-1-%EC%9E%90%EB%B0%94-%EB%A9%94%EB%AA%A8%EB%A6%AC-%EA%B5%AC%EC%A1%B0%EC%99%80-static-%ED%82%A4%EC%9B%8C%EB%93%9C%EC%9D%B4%EB%A1%A0-%EC%A0%95%EB%A6%AC
+* */
+
+
+package Backtracking;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class S_3234 {
     static int ans;
-    static int[] used;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -36,7 +54,7 @@ public class Main {
 
         if (leftSum >= totalSum - leftSum) {
             int leftChu = N - lev;
-            ans += Math.pow(2, leftChu) * factorial(leftChu);
+            ans += (int) (Math.pow(2, leftChu) * factorial(leftChu));
             return;
         }
 
