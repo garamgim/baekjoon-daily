9ea6e13befb6398b9454827cebba359c89c52672
garamgim <garamgim@gmail.com>
2024-03-16 21:28:01 +0900

    Add solution 1715

--- StackQueueDeque/B_G4_1715.java
+++ StackQueueDeque/B_G4_1715.java
@@ -10,16 +10,19 @@ import java.util.*;
 public class B_G4_1715 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+
         PriorityQueue<Long> nums = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
             nums.add(Long.parseLong(br.readLine()));
         }
-        long s = 0;
-        long prev;
-        long curr;
+
         if (N == 1) System.out.println(0);
         else {
+            long s = 0;
+            long prev;
+            long curr;
             while (!nums.isEmpty()) {
                 prev = nums.poll();
                 curr = prev + nums.poll();
