62fe648877228c632937820a8e3a1ffc0b384045
garamgim <garamgim@gmail.com>
2024-04-20 11:29:18 +0900

    Add solution 4781

--- DP/B_G4_4781.java
+++ DP/B_G4_4781.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 
 
 public class B_G4_4781 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
